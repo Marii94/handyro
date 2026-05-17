0658592c57db1b6a06bebc4a8c9c70e7d20c9474
Marii94 <serbumarian911@gmail.com>
2026-05-17 17:33:30 +0300

    Delete handyro-v5 directory

--- handyro-v5/handyro-v5-2/public/sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'handyro-v4';
-const ASSETS = ['/', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
